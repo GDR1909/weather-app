246102ebc71d313ec5286e2c7e0d5f41ef0e4543
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2025-02-24 11:29:44 +0100

    the api call is working

---
+++ src/app/weather/weather.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { WeatherService } from '../services/weather.service';
+
+@Component({
+  selector: 'app-weather',
+  standalone: true,
+  imports: [],
+  templateUrl: './weather.component.html',
+  styleUrl: './weather.component.scss'
+})
+export class WeatherComponent {
+  constructor(private weatherService: WeatherService) {
+    this.getWeather('Stuttgart');
+  }
+
+  getWeather(city: string) {
+    this.weatherService.getWeather(city).subscribe({
+      next: (data) => console.log('Wetterdaten:',data),
+      error: (err) => console.error('Fehler beim Abrufen der Wetterdaten:', err)
+    });
+  }
+}
