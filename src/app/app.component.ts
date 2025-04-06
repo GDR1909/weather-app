585a949c025edc78bba17d62622c4f51f751032c
GDR1909 <125650804+GDR1909@users.noreply.github.com>
2025-04-06 19:33:14 +0200

    installed the aos library and started to implent it to the components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, OnInit, Inject, PLATFORM_ID } from '@angular/core';
+import { CommonModule, isPlatformBrowser } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/components/header/header.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
+import AOS from 'aos';
+import 'aos/dist/aos.css'
 
 @Component({
   selector: 'app-root',
@@ -16,6 +18,17 @@ import { FooterComponent } from './shared/components/footer/footer.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'weather-app';
+
+  // constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
+
+  ngOnInit(): void {
+    AOS.init();
+
+    // if (isPlatformBrowser(this.platformId)) {
+    //   // Nur im Browser ausführen!
+    //   AOS.init();
+    // }
+  }
 }
